0d27c4b4f0660db0280e0108af9b550d4c579b83
Hayena <donovan.dekuiper@gmail.com>
2013-12-04 13:13:48 +0100

    Added tests for failures Added functionality to handle failures

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,13 +5,23 @@ import play.api.mvc._
 import play.api.libs.json.Json
 import models.Person
 import service.Service
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
 class Application extends Controller {
-  
+
+  implicit val reads = (
+    (__ \ 'latitude).read[Double] and
+    (__ \ 'longitude).read[Double]) tupled
+
   def search = Action(parse.json) { request =>
-    val latitude = (request.body \ "latitude").as[Double]
-    val longitude = (request.body \ "longitude").as[Double]
-    Ok(Json.obj("status" -> "OK", "people" -> Service.searchPeople(latitude, longitude)))
+
+    request.body.validate[(Double, Double)].map {
+      case (latitude, longitude) =>
+        Ok(Json.obj("status" -> "OK", "people" -> Service.searchPeople(latitude, longitude)))
+    }.recoverTotal {
+      e => BadRequest(JsError.toFlatJson(e))
+    }
   }
 }
 
--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -31,9 +31,24 @@ class ApplicationSpec extends Specification {
     "find no users within region" in new WithApplication {
       val json = Json.obj("longitude" -> 24.4, "latitude" -> 3.2)
       val search = requestJson(json, "/search")
+      status(search) must equalTo(OK)
       contentType(search) must beSome("application/json")
       val result = Json.parse(contentAsString(search))
       (result \ "people").as[List[JsValue]].length must beEqualTo(0)
+    }
+    
+    "BadRequest on invalid JSON" in new WithApplication {
+      val json = Json.obj("invalid" -> "request")
+      val search = requestJson(json, "/search")
+      status(search) must equalTo(BAD_REQUEST)
+      contentType(search) must beSome("application/json")
+      contentAsString(search) must contain("error.path.missing")
+    }
+    
+    "BadRequest on invalid data" in new WithApplication {
+      val xml = <location><latitude>54.2</latitude><longitude>5.2</longitude></location>
+      val search = route(FakeRequest(POST, "/search").withXmlBody(xml)).get
+      status(search) must equalTo(BAD_REQUEST)
     }
   }
 }
