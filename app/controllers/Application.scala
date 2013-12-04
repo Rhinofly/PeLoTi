4e612016c9ac6090fb598aa6b718b65a28673ace
Hayena <donovan.dekuiper@gmail.com>
2013-12-04 13:52:05 +0100

    Created test for creating a new person Created functions to store a new person into the database Refactored test and functions

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,11 +9,28 @@ import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
 class Application(service: Service) extends Controller {
-  
+
+  implicit val reads = (
+    (__ \ 'latitude).read[Double] and
+    (__ \ 'longitude).read[Double]) tupled
+
   def search = Action(parse.json) { request =>
-    val latitude = (request.body \ "latitude").as[Double]
-    val longitude = (request.body \ "longitude").as[Double]
-    Ok(Json.obj("status" -> "OK", "people" -> service.searchPeople(latitude, longitude)))
+
+    request.body.validate[(Double, Double)].map {
+      case (latitude, longitude) =>
+        Ok(Json.obj("status" -> "OK", "people" -> service.searchPeople(latitude, longitude)))
+    }.recoverTotal {
+      e => BadRequest(JsError.toFlatJson(e))
+    }
+  }
+
+  def create = Action(parse.json) { request =>
+    request.body.validate[(Double, Double)].map {
+      case (latitude, longitude) =>
+        Ok(Json.obj("status" -> "OK", "id" -> service.createPerson(latitude, longitude)))
+    }.recoverTotal {
+      e => BadRequest(JsError.toFlatJson(e))
+    }
   }
 }
 
--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,14 +1,14 @@
+import scala.concurrent.Future
+import org.junit.runner._
 import org.specs2.mutable._
 import org.specs2.runner._
-import org.junit.runner._
+import play.api.libs.json.JsObject
+import play.api.libs.json.JsValue
+import play.api.libs.json.JsValue
+import play.api.mvc.SimpleResult
 import play.api.test._
 import play.api.test.Helpers._
 import play.api.libs.json.Json
-import play.api.libs.json.JsValue
-import play.api.libs.json.JsValue
-import play.api.libs.json.JsObject
-import scala.concurrent.Future
-import play.api.mvc.SimpleResult
 
 @RunWith(classOf[JUnitRunner])
 class ApplicationSpec extends Specification {
@@ -36,7 +36,7 @@ class ApplicationSpec extends Specification {
       val result = Json.parse(contentAsString(search))
       (result \ "people").as[List[JsValue]].length must beEqualTo(0)
     }
-   
+    
     "BadRequest on invalid JSON" in new WithApplication {
       val json = Json.obj("invalid" -> "request")
       val search = requestJson(json, "/search")
@@ -49,6 +49,39 @@ class ApplicationSpec extends Specification {
       val xml = <location><latitude>54.2</latitude><longitude>5.2</longitude></location>
       val search = route(FakeRequest(POST, "/search").withXmlBody(xml)).get
       status(search) must equalTo(BAD_REQUEST)
+    }
+
+    "create a user" in new WithApplication {
+      val json = Json.obj("longitude" -> 54.24, "latitude" -> 5.23)
+      val create = requestJson(json, "/create")
+      contentType(create) must beSome("application/json")
+      val result = Json.parse(contentAsString(create))
+      (result \ "status").as[String] must contain("OK")
+    }
+    
+    "should find new user" in new WithApplication {
+      val json = Json.obj("longitude" -> 54.2, "latitude" -> 5.2)
+      val search = requestJson(json, "/search")
+      status(search) must equalTo(OK)
+      contentType(search) must beSome("application/json")
+      val result = Json.parse(contentAsString(search))
+      (result \ "people").as[List[JsValue]].length must beEqualTo(5)
+    }
+    
+    "new users should get diffrent id" in new WithApplication {
+      val json = Json.obj("longitude" -> 54.24, "latitude" -> 5.23)
+      val create1 = requestJson(json, "/create")
+      val create2 = requestJson(json, "/create")
+      println(contentAsString(create1))
+      println(contentAsString(create2))
+      contentType(create1) must beSome("application/json")
+      contentType(create2) must beSome("application/json")
+      val result1 = Json.parse(contentAsString(create1))
+      val result2 = Json.parse(contentAsString(create2))
+      
+      (result1 \ "status").as[String] must contain("OK")
+      (result2 \ "status").as[String] must contain("OK")
+      (result1 \ "id").as[Long] mustNotEqual((result2 \ "id").as[Long])
     }
   }
 }
